4d1ee0482d0ffdd61b5c4d492d1377a339ece461
Janessa Tran <tranjanessa@gmail.com>
2019-02-24 15:38:49 -0600

    upload changes in response to comments!

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,21 +1,25 @@
+$lowercase_letters = ('a'..'z').to_a
+$uppercase_letters = ('A'..'Z').to_a
+
+def get_shifted_idx(style, char, shift)
+  if style == 'lowercase'
+    shifted_idx = $lowercase_letters.index(char) + shift
+  else
+    shifted_idx = $uppercase_letters.index(char) + shift
+  end
+  if shifted_idx > $lowercase_letters.size
+    shifted_idx -= $lowercase_letters.size
+  end
+  return shifted_idx
+end
+
 def caesar_cipher(str, shift) 
-  lowercase_letters = Array('a'..'z')
-  uppercase_letters = Array('A'..'Z')
   new_str = ''
   str.each_char do |c|
-    if lowercase_letters.include?(c)
-      shifted_idx = lowercase_letters.index(c) + shift
-      # if the new idx is out of bounds, loop back to beginning for idx
-      if shifted_idx > 26
-        shifted_idx -= 26
-      end
-      new_str += lowercase_letters[shifted_idx]
-    elsif uppercase_letters.include?(c)
-      shifted_idx = uppercase_letters.index(c) + shift
-      if shifted_idx > 26
-        shifted_idx -= 26
-      end
-      new_str += uppercase_letters[shifted_idx]       
+    if $lowercase_letters.include?(c)
+      new_str += $lowercase_letters[get_shifted_idx('lowercase', c, shift)]
+    elsif $uppercase_letters.include?(c)
+      new_str += $uppercase_letters[get_shifted_idx('uppercase', c, shift)]
     else
       new_str += c       
     end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,14 +1,18 @@
 require "./caesar_cipher"
 
 describe "#caesar_cipher" do
+
   it "applies Caesar Cipher encoding to your string" do
     expect(caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
   end
+
   it "maintains special characters and upper/lower case" do
     expect(caesar_cipher("ABC?", 1)).to eq("BCD?")
   end
+  
   it "works in reverse!" do
     expect(caesar_cipher("abc", -1)).to eq("zab")
   end
+
 end
 
