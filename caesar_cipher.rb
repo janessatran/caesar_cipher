5fb0e27af87cbc2e252d27b617c4bb46d8ff833c
Janessa Tran <tranjanessa@gmail.com>
2019-02-26 19:32:23 -0600

    changes to simplify code and use each_with_object

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,28 +1,24 @@
-$lowercase_letters = ('a'..'z').to_a
-$uppercase_letters = ('A'..'Z').to_a
 
-def get_shifted_idx(style, char, shift)
-  if style == 'lowercase'
-    shifted_idx = $lowercase_letters.index(char) + shift
-  else
-    shifted_idx = $uppercase_letters.index(char) + shift
-  end
-  if shifted_idx > $lowercase_letters.size
-    shifted_idx -= $lowercase_letters.size
+def get_shifted_idx(letters_array, char, shift)
+    shifted_idx = letters_array.index(char) + shift
+  if shifted_idx > letters_array.size
+    shifted_idx -= letters_array.size
   end
   return shifted_idx
 end
 
 def caesar_cipher(str, shift) 
-  new_str = ''
-  str.each_char do |c|
-    if $lowercase_letters.include?(c)
-      new_str += $lowercase_letters[get_shifted_idx('lowercase', c, shift)]
-    elsif $uppercase_letters.include?(c)
-      new_str += $uppercase_letters[get_shifted_idx('uppercase', c, shift)]
+  cc_str = ''
+  lowercase_letters = ('a'..'z').to_a
+  uppercase_letters = ('A'..'Z').to_a
+  str.split("").each_with_object("") do |c| 
+    if lowercase_letters.include?(c)
+      cc_str << lowercase_letters[get_shifted_idx(lowercase_letters, c, shift)]
+    elsif uppercase_letters.include?(c)
+      cc_str << uppercase_letters[get_shifted_idx(uppercase_letters, c, shift)]
     else
-      new_str += c       
+      cc_str << c
     end
   end
-  return new_str
+  return cc_str
 end
